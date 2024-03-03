c6ec473f82775f57588247d405a2b550f4a21ec3
Builderman4 <ibrahim.syed5612@gmail.com>
2024-03-03 18:16:15 -0500

    Fixed viewcontroller added emojis

---
+++ EmojiDictionaryPartTwo/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  EmojiDictionaryPartTwo
+//
+//  Created by Ibrahim Syed on 2024-03-03.
+//
+
+import Foundation
+
+struct Emoji {
+    
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
---
+++ EmojiDictionaryPartTwo/EmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionaryPartTwo
+//
+//  Created by Ibrahim Syed on 2024-03-03.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
