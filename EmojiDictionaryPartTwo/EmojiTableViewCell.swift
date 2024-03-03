6b0cdd1623f23efff986a503519da8cd02c609de
Builderman4 <ibrahim.syed5612@gmail.com>
2024-03-03 18:16:39 -0500

    Commit

--- EmojiDictionaryPartTwo/EmojiTableViewCell.swift
+++ EmojiDictionaryPartTwo/EmojiTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
 
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,5 +23,13 @@ class EmojiTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
 
 }
+
+
